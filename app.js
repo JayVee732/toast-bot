f53ac8d8d9a88a60040e6efcac413d9d52464497
Jamie Higgins <jamie.v732@gmail.com>
2021-08-05 00:41:46 +0100

    Added eslint

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-require("dotenv").config();
-const Discord = require("discord.js");
+require('dotenv').config();
+const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
-var today = new Date();
-var wkday = today.getDay();
+const today = new Date();
+const wkday = today.getDay();
 
 bot.login(TOKEN);
 
-bot.on("ready", () => {
+bot.on('ready', () => {
 	console.log(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on("message", (msg) => {
-	if (msg.content.includes("toast")) {
-		thursday(msg);
+bot.on('message', (msg) => {
+	if (msg.content.includes('toast')) {
+		friday(msg);
 	}
 });
-function thursday(msg) {
+
+function friday(msg) {
 	if (wkday == 5) {
-		msg.reply("Time for T O A S T!");
+		msg.reply('Time for T O A S T!');
 	} else {
-		msg.reply("You may eat toast, but it is not time for T O A S T yet.");
+		msg.reply('You may eat toast, but it is not time for T O A S T yet.');
 	}
 }
