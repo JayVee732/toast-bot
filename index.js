525c0ef59a4356ecb45461f5486c1bb128261ea3
JayVee732 <jamie.v732@gmail.com>
2022-03-04 21:46:33 +0000

    Update to use dynamic channel id

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const TOKEN = process.env.TOKEN;
 const cron = require('node-cron');
-let channelId = '869922821839650860';
+let channelId = process.env.CHANNELID;
 
 client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -36,14 +36,14 @@ function changeChannel(name, topic) {
 client.login(TOKEN);
 
 // Change to Toast Zone at 00:00 on Friday
-//cron.schedule('30 0 * * 5', function () {
+//cron.schedule('0 0 * * 5', function () {
 cron.schedule('42 21 * * *', function () {
 	console.log('Time for T O A S T');
 	changeChannel('🍞-toast-zone-🍞', 'Toast');
 });
 
 // Change to Pizza Zone at 00:00 on Saturday
-//cron.schedule('* * * * *', function () {
+//cron.schedule('0 0 * * 6', function () {
 cron.schedule('43 21 * * *', function () {
 	console.log('Return to Pizza');
 	changeChannel('🍕-pizza-zone-🍕', 'Pizza');
