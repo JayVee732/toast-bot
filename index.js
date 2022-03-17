ff70542102e233f889fb248916e49bf1727085f8
Jamie Higgins <jamie.v732@gmail.com>
2022-03-17 22:17:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function changeChannel(name, topic) {
 client.login(TOKEN);
 
 // Change to Toast Zone at 00:00 on Friday
-cron.schedule('0 0 * * 5', function () {
+cron.schedule('22 21 * * 5', function () {
 	console.log('Time for T O A S T');
 	changeChannel('🍞-toast-zone-🍞', 'Toast');
 });
